dde0785594330c8a42495938b1d7cad63d2bbe20
Bryan Woodhams <Woodhamsbd@gmail.com>
2016-02-24 16:00:16 -0800

    init bootstrap and user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+	#FUll Title on Page Basis
+	def full_title(page_title = '')
+		base_title = "Rails_Rehash"
+		if page_title.empty?
+			base_title
+		else
+			page_title + " | " + base_title
+		end
+	end
+
 end
